0f45febf7ccd20bff6f45a035fa54903e1c453c2
Guan Jinbiao <Novorizon@hotmail.com>
2026-05-19 23:14:49 +0800

    Convert tower info panel to UIPanel

--- Assets/Game/UI/TowerDefense/Runtime/TowerInfoPanel.cs
+++ Assets/Game/UI/TowerDefense/Runtime/TowerInfoPanel.cs
@@ -1,86 +1,140 @@
 using System;
 using TMPro;
+using UI;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Game
 {
-    public sealed class TowerInfoPanel : MonoBehaviour
+    public sealed class TowerInfoPanel : UIPanel
     {
-        [SerializeField] private CanvasGroup canvasGroup;
-        [SerializeField] private Image towerIconImage;
-        [SerializeField] private TMP_Text towerNameText;
-        [SerializeField] private TMP_Text levelText;
-        [SerializeField] private TMP_Text attackText;
-        [SerializeField] private TMP_Text attackAddText;
-        [SerializeField] private TMP_Text rangeText;
-        [SerializeField] private TMP_Text speedText;
-        [SerializeField] private TMP_Text upgradeCostText;
-        [SerializeField] private TMP_Text sellGoldText;
-        [SerializeField] private Button upgradeButton;
-        [SerializeField] private Button sellButton;
+        [SerializeField]
+        private CanvasGroup canvasGroup;
+
+        [SerializeField]
+        private Image towerIconImage;
+
+        [SerializeField]
+        private TMP_Text towerNameText;
+
+        [SerializeField]
+        private TMP_Text levelText;
+
+        [SerializeField]
+        private TMP_Text attackText;
+
+        [SerializeField]
+        private TMP_Text attackAddText;
+
+        [SerializeField]
+        private TMP_Text rangeText;
+
+        [SerializeField]
+        private TMP_Text speedText;
+
+        [SerializeField]
+        private TMP_Text upgradeCostText;
+
+        [SerializeField]
+        private TMP_Text sellGoldText;
+
+        [SerializeField]
+        private Button upgradeButton;
+
+        [SerializeField]
+        private Button sellButton;
 
         private int selectedTowerId;
 
         public event Action<int> UpgradeClicked;
         public event Action<int> SellClicked;
 
-        private void Awake()
+        protected override void OnCreate()
         {
             if (upgradeButton != null)
             {
                 upgradeButton.onClick.AddListener(OnUpgradeClicked);
             }
+
             if (sellButton != null)
             {
                 sellButton.onClick.AddListener(OnSellClicked);
             }
+
             Hide();
+        }
+
+        protected override void OnDestroyed()
+        {
+            if (upgradeButton != null)
+            {
+                upgradeButton.onClick.RemoveListener(OnUpgradeClicked);
+            }
+
+            if (sellButton != null)
+            {
+                sellButton.onClick.RemoveListener(OnSellClicked);
+            }
+
+            UpgradeClicked = null;
+            SellClicked = null;
         }
 
         public void Show(TdTowerRuntimeInfo info)
         {
             selectedTowerId = info.TowerId;
+
             if (towerIconImage != null)
             {
                 towerIconImage.sprite = info.Icon;
+                towerIconImage.enabled = info.Icon != null;
             }
+
             if (towerNameText != null)
             {
                 towerNameText.text = info.Name;
             }
+
             if (levelText != null)
             {
                 levelText.text = $"等级 {info.Level}";
             }
+
             if (attackText != null)
             {
                 attackText.text = info.Attack.ToString();
             }
+
             if (attackAddText != null)
             {
                 attackAddText.text = info.AttackAdd > 0 ? $"+{info.AttackAdd}" : string.Empty;
             }
+
             if (rangeText != null)
             {
                 rangeText.text = $"{info.Range:0.#} 格";
             }
+
             if (speedText != null)
             {
                 speedText.text = $"{info.AttackInterval:0.#} 秒";
             }
+
             if (upgradeCostText != null)
             {
                 upgradeCostText.text = info.UpgradeCost.ToString();
             }
+
             if (sellGoldText != null)
             {
                 sellGoldText.text = info.SellGold.ToString();
             }
+
             if (upgradeButton != null)
             {
                 upgradeButton.interactable = info.CanUpgrade;
             }
+
             SetVisible(true);
         }
 
@@ -97,11 +151,10 @@ namespace Game
                 canvasGroup.alpha = visible ? 1f : 0f;
                 canvasGroup.interactable = visible;
                 canvasGroup.blocksRaycasts = visible;
+                return;
             }
-            else
-            {
-                gameObject.SetActive(visible);
-            }
+
+            gameObject.SetActive(visible);
         }
 
         private void OnUpgradeClicked()
