0130624449dbe63c1a23563ce7053d16c55b7792
Guan Jinbiao <Novorizon@hotmail.com>
2026-05-22 16:17:57 +0800

    Add target info ui model

--- Assets/Scripts/Game/UI/TowerDefense/TdUiModels.cs
+++ Assets/Scripts/Game/UI/TowerDefense/TdUiModels.cs
@@ -21,6 +21,39 @@ namespace Game
         public int Count;
     }
 
+    public enum TdTargetInfoType
+    {
+        None = 0,
+        Base = 1,
+        Tower = 2,
+        Npc = 3,
+        Item = 4,
+    }
+
+    [Serializable]
+    public struct TdTargetRuntimeInfo
+    {
+        public TdTargetInfoType Type;
+        public int TargetId;
+        public string Name;
+        public string Description;
+        public Sprite Icon;
+
+        public int Level;
+        public int CurrentHp;
+        public int MaxHp;
+
+        public int Attack;
+        public int AttackAdd;
+        public float Range;
+        public float AttackInterval;
+
+        public int UpgradeCost;
+        public int SellGold;
+        public bool CanUpgrade;
+        public bool CanSell;
+    }
+
     [Serializable]
     public struct TdTowerRuntimeInfo
     {
