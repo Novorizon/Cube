b69fe795088d64f0bd0764797553c0c0a0116037
Guan Jinbiao <Novorizon@hotmail.com>
2026-05-22 23:43:44 +0800

    Add skill system enums

---
+++ Assets/Scripts/Skill/Core/SkillEnums.cs
@@ -0,0 +1,81 @@
+using System;
+
+namespace Game.Skill
+{
+    [Flags]
+    public enum SkillAbilityBehavior
+    {
+        None = 0,
+        NoTarget = 1,
+        UnitTarget = 2,
+        PointTarget = 4,
+        Passive = 8,
+        Toggle = 16,
+        Channel = 32,
+        Aoe = 64
+    }
+
+    public enum SkillTargetTeam
+    {
+        None = 0,
+        Friendly = 1,
+        Enemy = 2,
+        Both = 3
+    }
+
+    public enum SkillTargetType
+    {
+        None = 0,
+        Caster = 1,
+        Unit = 2,
+        Point = 3,
+        Area = 4,
+        CurrentTargets = 5
+    }
+
+    public enum SkillActionType
+    {
+        None = 0,
+        Damage = 1,
+        Heal = 2,
+        ApplyModifier = 3,
+        FireEvent = 4
+    }
+
+    public enum SkillModifierType
+    {
+        None = 0,
+        Property = 1,
+        Periodic = 2,
+        State = 3,
+        Aura = 4
+    }
+
+    public enum SkillModifierPropertyType
+    {
+        None = 0,
+        MoveSpeedPercent = 1,
+        AttackSpeedPercent = 2,
+        DamageBonus = 3,
+        ArmorBonus = 4,
+        IncomingDamagePercent = 5,
+        OutgoingDamagePercent = 6
+    }
+
+    public enum SkillUnitState
+    {
+        None = 0,
+        Stunned = 1,
+        Silenced = 2,
+        Rooted = 3,
+        Invulnerable = 4
+    }
+
+    public enum SkillDamageType
+    {
+        None = 0,
+        Physical = 1,
+        Magical = 2,
+        Pure = 3
+    }
+}
