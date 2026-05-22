2babd01160231099091e841984d2bff45edb1774
Guan Jinbiao <Novorizon@hotmail.com>
2026-05-22 23:46:30 +0800

    Add skill modifier instance

---
+++ Assets/Scripts/Skill/Modifiers/SkillModifierInstance.cs
@@ -0,0 +1,15 @@
+namespace Game.Skill
+{
+    public sealed class SkillModifierInstance
+    {
+        public int ModifierId;
+        public SkillModifierData Data;
+        public ISkillUnit Caster;
+        public ISkillUnit Parent;
+        public float Duration;
+        public float TimeLeft;
+        public float Interval;
+        public float IntervalTimer;
+        public int StackCount = 1;
+    }
+}
