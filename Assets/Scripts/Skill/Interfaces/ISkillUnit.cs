5c557ffa9ff678b4f38ff5bcda134f833223ec10
Guan Jinbiao <Novorizon@hotmail.com>
2026-05-22 23:44:01 +0800

    Add skill system interfaces

---
+++ Assets/Scripts/Skill/Interfaces/ISkillUnit.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace Game.Skill
+{
+    public interface ISkillUnit
+    {
+        int RuntimeId { get; }
+        int TeamId { get; }
+        bool IsAlive { get; }
+        Vector3 Position { get; }
+
+        float GetProperty(SkillModifierPropertyType propertyType);
+        bool HasState(SkillUnitState state);
+        void TakeDamage(SkillDamageInfo damageInfo);
+        void Heal(SkillHealInfo healInfo);
+    }
+
+    public interface ISkillWorld
+    {
+        float Time { get; }
+        void FindUnits(Vector3 center, float radius, SkillTargetFilter filter, SkillTargetResult result);
+        bool HasLineOfSight(ISkillUnit caster, Vector3 position);
+    }
+
+    public interface ISkillResourceOwner
+    {
+        bool HasResource(int resourceId, int count);
+        bool TryConsumeResource(int resourceId, int count);
+    }
+
+    public interface ISkillEffectService
+    {
+        void PlayEffect(string location, Vector3 position);
+        void PlayEffect(string location, ISkillUnit target);
+        void PlaySound(string location, Vector3 position);
+    }
+}
